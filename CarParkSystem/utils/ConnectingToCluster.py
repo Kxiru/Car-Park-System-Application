82ea4e9b34a9d89eff0d6514186aee284e2610ff
UWantMyName <stefantanasa13@gmail.com>
2020-02-06 15:16:07 +0000

    Added a class Ticket and created methods to add to table and to get all the items from a specific table

--- CarParkSystem/utils/ConnectingToCluster.py
+++ CarParkSystem/utils/ConnectingToCluster.py
@@ -1,8 +1,6 @@
 import pymongo
-import datetime
 
-my_client = pymongo.MongoClient("mongodb+srv://Admin:admin123@cs1813namedb-maax9.mongodb.net/test?retryWrites=true&w=majority"
-)
+my_client = pymongo.MongoClient('mongodb+srv://Stefan:stefan123@cs1813namedb-maax9.mongodb.net/test?retryWrites=true&w=majority')
 
 try:
     print("MongoDB version is %s" % my_client.server_info()['version'])
@@ -10,43 +8,50 @@ except pymongo.errors.OperationFailure as error:
     print(error)
     quit(1)
 
-my_database = my_client.CarParkDatabase
-my_collection = my_database.TicketsTable
+class Ticket:
+    def __init__(self, id, nameOfCustomer, ticketType, ticketPrice, timeStayedHRs, date, inTime, outTime):
+        self.id = id;
+        self.nameOfCustomer = nameOfCustomer
+        self.ticketType = ticketType
+        self.ticketPrice = ticketPrice
+        self.timeStayedHRs = timeStayedHRs
+        self.date = date
+        self.inTime = inTime
+        self.outTime = outTime
 
-records = my_collection.find()
-for item in records:
-    print(item)
+
+def GetAllItems(Database, Table):
+    Collection = Table.find()
+
+    for item in Collection:
+        print(item)
+
+def AddItem(Database, Table, ticket):
+    try:
+        Table.insert_one({
+            "_id": ticket.id,
+            "Name_of_Customer": ticket.nameOfCustomer,
+            "TicketType": ticket.ticketType,
+            "TicketPrice": ticket.ticketPrice,
+            "TimeStayedHRs": ticket.timeStayedHRs,
+            "Date": ticket.date,
+            "InTime": ticket.inTime,
+            "OutTime": ticket.outTime
+        })
+    except NotImplementedError:
+        print("Could not do operation of adding ticket.")
 
 
-#////////////////////////////
-# This segment of code refers to how to query data from the database.
-# my_cursor = my_collection.find()
-
-# for item in my_cursor:
-#     print(item["name"])
-
-#////////////////////////////
-# How to insert into the database. You will get an error if you insert records with pre-existing IDs (OR other fields)
-# my_collection.insert_many([
-#     {
-#         "_id": 1,
-#         "name": "Bill W",
-#         "ticketPrice": 3.20,
-#         "timeStayedHRs": 4,
-#         "Date": datetime.datetime.utcnow(),
-#     },{
-#         "_id": 2,
-#         "name": "Jen C",
-#         "ticketPrice": 3.20,
-#         "timeStayedHRs": 3,
-#         "Date": datetime.datetime(2020,2,4),
-#     },{
-#         "_id": 3,
-#         "name": "Karl J",
-#         "ticketPrice": 3.20,
-#         "timeStayedHRs": 4,
-#         "Date": datetime.datetime(2020,2,5),
-#     }
-# ])
-
+Database = my_client.CarParkDatabase
+#info = Database.getCollectionInfos()
+#Database.showCollections()
+TicketTable = Database.TicketsTable
+#x = TicketTable.count()
+#t1 = Ticket(x + 1, "Donald T", "Customer", 3, 1, "02.01.2019", "16:00", "17:00")
+#t2 = Ticket(x + 2, "Adam G.", "Customer", 2.5, 3, "05.04.2019", "10:00", "13:00")
+#t3 = Ticket(x + 3, "Otis H.", "Customer", 3, 1.5, "19.08.2018", "14:00", "15:30")
+#AddItem(Database, TicketTable, t1)
+#AddItem(Database, TicketTable, t2)
+#AddItem(Database, TicketTable, t3)
+GetAllItems(Database, TicketTable)
 
